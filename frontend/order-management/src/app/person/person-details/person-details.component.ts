56382df0f270a384a97010df695d76c19ba73507
GabrielFaria23 <gabrielfariapta01@gmail.com>
2020-12-30 16:11:57 -0300

    Details implemented

--- frontend/order-management/src/app/person/person-details/person-details.component.ts
+++ frontend/order-management/src/app/person/person-details/person-details.component.ts
@@ -8,14 +8,29 @@ import { PersonService } from '../person.service';
   templateUrl: './person-details.component.html',
   styleUrls: ['./person-details.component.css']
 })
-export class PersonDetailsComponent implements OnInit {
+export class PersonDetailsComponent implements OnInit {  
+
+  id: number;
+  person: Person;
 
   constructor(private personService: PersonService,
               private route: ActivatedRoute,
               private router: Router) { }
 
   ngOnInit(){
-    
+    this.id = this.route.snapshot.params['id'];
+    this.personService.getPersonById(this.id)
+      .subscribe(
+        data =>{ 
+          this.person = data;
+          console.log(this.person);
+          
+        }, error=> console.log(error)
+      )
+  }
+
+  BackToList(){
+    this.router.navigate(['people'])
   }
 
 }
